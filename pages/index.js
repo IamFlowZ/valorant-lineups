5601f3e27ab0a68cbf6e3204de49a8732edea314
IamFlowZ <dakota.lewallen17@gmail.com>
2021-12-10 20:06:36 -0500

    cleaning out index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import aws from 'aws-sdk';
 import Head from 'next/head'
 import Image from 'next/image'
@@ -21,41 +23,18 @@ import {
 } from '../components/enums';
 
 export default function Home({data}) {
-  console.log(data);
   const router = useRouter();
-  const [ourData, setOurData] = useState({});
   const [map, setMap] = useState('any');
   const [agent, setAgent] = useState('any');
-  const [location, setLocation] = useState('any');
-  const [mapLocations, setMapLocations] = useState([]);
-  const [attack, setAttack] = useState('either');
-  const [stage, setStage] = useState('any');
-  const [utilityOrWallbang, setUtilOrWallbang] = useState('either');
-  const [utilityType, setUtilType] = useState('any');
-  const [ability, setAbility] = useState('any');
-  const [abilities, setAbilities] = useState([]);
-  const [difficulty, setDifficulty] = useState(0);
-  const [usefulness, setUsefulness] = useState(0);
 
   // handle setting any value that isn't map/agent as url query params
   // handle setting map/agent as path params.
   // with both it's arguable that you could do them inside onChange, but maybe inside useEffect leads to less duplication?
   useEffect(() => {
-    if (agent !== 'any') {
-      setAbilities(ABILITY_MAPPING[agent]);
-    } else {
-      setAbilities([]);
-    }
-    console.log(map)
-    if (map !== 'any') {
-      setMapLocations(MAP_LOCATIONS[map]);
-    } else {
-      setMapLocations([]);
-    }
     if (map !== 'any' || agent !== 'any') {
       router.push(`${map}/${agent}`)
     }
-  }, [agent, map, ourData, router])
+  }, [agent, map, router])
 
   return (
     <div className={styles.container}>
@@ -78,49 +57,30 @@ export default function Home({data}) {
           <h1 className={styles.title}>
             Valorant-Lineups.gg
           </h1>
+          <h2 style={{textAlign: 'center'}}>The best lineup study tool</h2>
           <div>
             <form style={{display: 'flex', flexDirection: 'column'}} className="pure-form">
-              <section style={{display: 'flex'}}>
+              <section style={{display: 'flex', alignItems: 'center'}}>
                 <Select item='map' itemState={map} setItemState={setMap} selectOptions={MAPS} />
-                {/* add some logic to include locations based on map selection */}
                 <Select item='agent' itemState={agent} setItemState={setAgent} selectOptions={AGENTS} />
+                <FontAwesomeIcon icon={faArrowLeft} style={{heigh: '24px', width: '24px'}}/>
+                <p>get started here...</p>
               </section>
-              { map !== 'any' && agent !== 'any' ?
-                <>
-                <section style={{display: 'flex'}}>
-                  <Select item='attack' itemState={attack} setItemState={setAttack} selectOptions={ATKORDFND} />
-                  <Select item='stage' itemState={stage} setItemState={setStage} selectOptions={ROUND_STAGES} />
-                  { map !== 'any' ?
-                    <Select item='location' itemState={location} setItemState={setLocation} selectOptions={mapLocations} />
-                    : null
-                  }
-                </section>
-                <section style={{display: 'flex'}}>
-                  <Select item='utility' itemState={utilityOrWallbang} setItemState={setUtilOrWallbang} selectOptions={UTIL_OR_WALLBANG} />
-                  { utilityOrWallbang !== 'wallbang' ?
-                  <>
-                    <Select item='utilityType' itemState={utilityType} setItemState={setUtilType} selectOptions={UTILITY_TYPE} />
-                    { agent !== 'all' && abilities.length > 0 ? 
-                      <Select item='ability' itemState={ability} setItemState={setAbility} selectOptions={abilities} />
-                      : null
-                    }
-                    </>
-                    : null
-                  }
-                </section>
-                <section style={{display: 'flex'}}>
-                  <Select item='difficulty' itemState={difficulty} setItemState={setDifficulty} selectOptions={DIFFICULTY} />
-                  <Select item='usefulness' itemState={usefulness} setItemState={setUsefulness} selectOptions={USEFULNESS} />
-                </section>
-                </>
-              : null}
             </form>
           </div>
         </div>
-
-        {/* <hr className="solid" style={{width: 'calc(100% - 15rem)'}} /> */}
         
-        {data.map((lineup, i) => <Lineup key={i} lineup={lineup} />)}
+        <div style={{
+          backgroundColor: 'rgb(var(--primary-color))',
+          marginBottom: '0.5rem',
+          borderRadius: '1rem',
+          padding: '1rem 2.5rem',
+
+          width: 'calc(100% - 15rem)',
+        }}>
+          <h1>Welcome!</h1>
+          <p>This is Valorant Lineups.gg</p>
+        </div>
       </main>
 
       <footer className={styles.footer}>
